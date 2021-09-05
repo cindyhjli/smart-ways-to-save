5ee95c68c288ddd8c552c3a719ed129f3aed7ecd
daniel-wang-design <business.danielwang@gmail.com>
2021-09-05 13:19:23 -0400

    google fonts

--- smart_ways_to_save/lib/main.dart
+++ smart_ways_to_save/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'Home.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(Test());
@@ -9,7 +10,9 @@ class Test extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(backgroundColor: Colors.white),
+      theme: ThemeData(
+          backgroundColor: Colors.white,
+          fontFamily: GoogleFonts.montserrat().fontFamily),
       debugShowCheckedModeBanner: false,
       debugShowMaterialGrid: false,
       home: Home(),
